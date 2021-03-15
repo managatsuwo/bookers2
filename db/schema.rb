6dd816b028f40c4542ec4fc81bb59bcfb78859b6
“managatsuwo” <mystknr.macbook13@gmail.com>
2021-03-15 11:38:41 +0000

    2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_093340) do
+ActiveRecord::Schema.define(version: 2021_03_15_105336) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.string "image_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
