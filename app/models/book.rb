63f503f1c0bdd0286da072452ceadae0cb8d4c07
“managatsuwo” <mystknr.macbook13@gmail.com>
2021-04-08 09:19:28 +0000

    応用課題6　いいねの非同期処理　完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,21 +5,22 @@ class Book < ApplicationRecord
   validates :title, presence:true
   validates :body, presence:true
   validates :body,    length: { maximum: 200 }
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
   def self.looks(searaches,word)
-                if searaches == "forward_match"
-                        @books = Book.where("body LIKE?","#{word}%")
-                elsif searaches == "backward_match"
-                        @books = Book.where("body LIKE?","%#{word}")
-                elsif searaches== "perfect_match"
-                        @books = Book.where("body=?","#{word}")
-                elsif searaches == "partial_match"
-                        @books = Book.where("body LIKE?","%#{word}%")
-                else
-                        @books = Book.all
-                end
+    if searaches == "forward_match"
+            @books = Book.where("body LIKE?","#{word}%")
+    elsif searaches == "backward_match"
+            @books = Book.where("body LIKE?","%#{word}")
+    elsif searaches== "perfect_match"
+            @books = Book.where("body=?","#{word}")
+    elsif searaches == "partial_match"
+            @books = Book.where("body LIKE?","%#{word}%")
+    else
+            @books = Book.all
+    end
   end
 end
